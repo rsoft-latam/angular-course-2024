cbaae9b4276aa0113a2c3ac76ef93f863e275c32
rsoft-latam <rikiricardo16@hotmail.com>
2024-09-20 20:16:41 -0400

    [20-09-2024] private and public functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title:number = 10;
+
+  constructor(){
+   console.log('subtract ', this.subtract(8,4))
+  }
+
+  public sum(num1: number, num2: number): number {
+    return num1 + num2
+  }
+
+  private subtract(num1: number, num2: number):number{
+    return num1 - num2
+  }
+
 }
