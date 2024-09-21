7f98abeb72bef1c84a305df03e6fe35c2cc4c7de
rsoft-latam <rikiricardo16@hotmail.com>
2024-09-20 21:30:42 -0400

    [20-10-2024] interface and js operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface IPerson {
+  name:string
+  lastName: string
+  age?: number
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -8,11 +14,24 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title:number = 10;
+  animals:string[] = ['a','b','c','d','e','f','g']
+
+  person: IPerson = {
+    name: 'a',
+    lastName: 'b'
+  }
 
   constructor(){
    console.log('subtract ', this.subtract(8,4))
+
+    console.log('MAP:', this.animals.map( (animal:string) => ( animal + 'new')    ))
+    console.log('FOREACH:', this.animals.forEach( (animal) => ( animal + 'new')    ))
+    console.log('FIND', this.animals.find((animal)=>  animal === 'z'))
+    console.log('FILTER', this.animals.filter((animal)=>  animal === 'z'))
+    console.log('INDEXOF', this.animals.indexOf('c'))
   }
 
   public sum(num1: number, num2: number): number {
@@ -29,5 +48,25 @@ export class AppComponent {
       console.log('person =', persons[i])
     }
   }
+
+
+
+  //  function sumar(){
+  //   return 1 + 2;
+  //  }  
+
+  //  const suma = () => {
+  //   return 1 + 2
+  //  }
+
+
+  //  function resta(){
+  //   return 'hola' + a
+  //  }
+
+  // arrow functions
+  //  const resta = () => ('hola' + a)
+
+
 
 }
