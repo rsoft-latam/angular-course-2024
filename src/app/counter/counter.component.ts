ad8b411a93d1ee57ad737bf973374d86cd1be3b0
Ricardo Pari <rikiricardo16@hotmail.com>
2024-10-03 20:33:38 -0400

    [30-10-2024] phases in afterRender and afterNextRender

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -10,11 +10,34 @@ import { Component, afterRender, afterNextRender } from '@angular/core';
 export class CounterComponent {
 
   sum: number = 0
+  appBackground: string = 'red'
 
   constructor() {
-    afterRender(() => {
-      console.log('AFTER RENDER: ', this.sum)
-    })
+    afterRender({
+      earlyRead: () => {
+        console.log('INTO earlyRead')
+        const currentAppColor = this.appBackground
+        return 'From earlyRead: ' + currentAppColor
+      },
+      mixedReadWrite: (props) => {
+        console.log('INTO mixedReadWrite ', props)
+        if(props.indexOf('red') > -1) {
+          this.appBackground = 'green'
+        } else {
+          this.appBackground = 'red'
+        }
+        return 'From mixedReadWrite: ' + this.appBackground
+      },
+      write:(props) => {
+        console.log('INTO write ', props)
+        document.body.style.backgroundColor = this.appBackground
+        return 'FROM write: ' + this.appBackground
+      },
+      read:(props) => {
+        console.log('INTO read ', props)
+        const newBackground = this.appBackground
+      },
+  })
 
     afterNextRender(() => {
       console.log('AFTER NEXT RENDER: ', this.sum)
