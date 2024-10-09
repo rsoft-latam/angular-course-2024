f66186ba51c23b67880bb11b3943d8c4c43e1be0
Ricardo Pari <rikiricardo16@hotmail.com>
2024-10-08 21:01:12 -0400

    [08-10-2024] observables first example, concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,97 +1,114 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { UserCardComponent } from './user-card/user-card.component';
-import { CalculatorComponent } from './calculator/calculator.component';
-import { CommonModule } from '@angular/common';
-import { CounterComponent } from './counter/counter.component';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { UserCardComponent } from "./user-card/user-card.component";
+import { CalculatorComponent } from "./calculator/calculator.component";
+import { CommonModule } from "@angular/common";
+import { CounterComponent } from "./counter/counter.component";
+import { filter, from, map, tap } from "rxjs";
 
 interface IPerson {
-  name: string
-  lastName: string
-  age?: number
+  name: string;
+  lastName: string;
+  age?: number;
 }
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, UserCardComponent, CalculatorComponent,CommonModule, CounterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    RouterOutlet,
+    UserCardComponent,
+    CalculatorComponent,
+    CommonModule,
+    CounterComponent,
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
-
 export class AppComponent {
+  users = [
+    { name: "abc", email: "abc@gmail.com" },
+    { name: "dfg", email: "dfg@gmail.com" },
+  ];
+  selectedUser: any = this.users[0];
 
-  users = [{ name: 'abc', 'email': 'abc@gmail.com' }, { name: 'dfg', 'email': 'dfg@gmail.com' }]
-  selectedUser:any = this.users[0];
+  userCardCreated: boolean = true;
 
-  userCardCreated: boolean = true
-
-  result:number = 0;
+  result: number = 0;
   title: number = 10;
-  animals: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+  animals: string[] = ["a", "b", "c", "d", "e", "f", "g"];
 
   person: IPerson = {
-    name: 'Juan',
-    lastName: 'Perez',
-    age: 25
-  }
-  students:number[] = [1,2,3,4,5,6]
-  parents:number[] = [7,8,9,10]
+    name: "Juan",
+    lastName: "Perez",
+    age: 25,
+  };
+  students: number[] = [1, 2, 3, 4, 5, 6];
+  parents: number[] = [7, 8, 9, 10];
 
-  var1 = 0
-  var2 = null
-  var3 = 'hola'
+  var1 = 0;
+  var2 = null;
+  var3 = "hola";
+
+  youtube = from([1, 2, 3, 4, 5, 6]);
 
   constructor() {
-    const { name, age } = this.person
-    //console.log('desestructuracion: ', name, age)
-    let both = [...this.students, ...this.parents]
-    //console.log('spreed operator:', both)
-    //console.log('REST operator:', this.sum(2,4,6))
+    const { name, age } = this.person;
+    let both = [...this.students, ...this.parents];
 
-    //console.log('Nullish Coalesing:',  this.var2 ?? this.var3  )
-    //console.log('OR:', this.var2 || this.var1)
-
-
-    //console.log('subtract ', this.subtract(8, 4))
-    //console.log('MAP:', this.animals.map((animal: string) => (animal + 'new')))
-    //console.log('FOREACH:', this.animals.forEach((animal) => (animal + 'new')))
-    //console.log('FIND', this.animals.find((animal) => animal === 'z'))
-    //console.log('FILTER', this.animals.filter((animal) => animal === 'z'))
-    //console.log('INDEXOF', this.animals.indexOf('c'))
+    this.youtube.subscribe((res) => {
+      console.log("SUSCRIBER 1: ", res);
+    });
   }
-  public sum(...persons:number[]){
+  public sum(...persons: number[]) {
     //return persons[0] + persons[1]
     return persons.reduce(
-      (acumulador, valorActual) => (acumulador + valorActual), 10
-    )
+      (acumulador, valorActual) => acumulador + valorActual,
+      10
+    );
+  }
+
+  addVideo() {
+    this.youtube
+      .pipe(
+        map((res: number) => {
+          //console.log("MAP OPERATOER RXJS: ", res);
+          if (res % 2 === 0) {
+            return res;
+          } else {
+            return null
+          }
+        }),
+        tap((res)  => {console.log('VALUE: ', res)}),
+        filter((res: number | null) => res !== null),
+      )
+      .subscribe((res) => {
+        console.log("SUSCRIBER 2: ", res);
+      });
   }
 
   public sum2(num1: number, num2: number): number {
-    return num1 + num2
+    return num1 + num2;
   }
 
   private subtract(num1: number, num2: number): number {
-    return num1 - num2
+    return num1 - num2;
   }
 
   public getArray(): void {
-    const persons: number[] = [1, 2, 3, 4, 5]
+    const persons: number[] = [1, 2, 3, 4, 5];
     for (let i = 0; i < persons.length; i++) {
       //console.log('person =', persons[i])
     }
   }
 
-
-
   //  function sumar(){
   //   return 1 + 2;
-  //  }  
+  //  }
 
   //  const suma = () => {
   //   return 1 + 2
   //  }
-
 
   //  function resta(){
   //   return 'hola' + a
@@ -100,13 +117,12 @@ export class AppComponent {
   // arrow functions
   //  const resta = () => ('hola' + a)
 
-  public receiveData(data:any){
-    console.log('Print in father component: ', data)
+  public receiveData(data: any) {
+    console.log("Print in father component: ", data);
   }
 
-  public onResult(event:any){
-    console.log('event from child:', event)
-    this.result = event ?? 0
+  public onResult(event: any) {
+    console.log("event from child:", event);
+    this.result = event ?? 0;
   }
-
 }
